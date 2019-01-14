68df9b0dd44488e99f068c206a98e21041dee9ca
amcaar <amcaar@i3m.upv.es>
2019-01-14 15:42:57 +0100

    allow build tests to the global role

--- scripts_jenkins/users_management/add_global_role.sh
+++ scripts_jenkins/users_management/add_global_role.sh
@@ -4,4 +4,4 @@ ROLE_NAME=$2
 USER=$3
 PASS=$4
 
-curl --user ${USER}:${PASS} ${JENKINS_ROOT_URL}/role-strategy/strategy/addRole --data "type=globalRoles&roleName=${ROLE_NAME}&permissionIds=hudson.model.Hudson.Read&overwrite=true"
+curl --user ${USER}:${PASS} ${JENKINS_ROOT_URL}/role-strategy/strategy/addRole --data "type=globalRoles&roleName=${ROLE_NAME}&permissionIds=hudson.model.Hudson.Read,hudson.model.Item.Build&overwrite=true"
