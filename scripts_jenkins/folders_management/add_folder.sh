83e06b56780c00eb8c9f0529525c2f357d13c2fd
amcaar <amcaar@i3m.upv.es>
2019-01-16 15:21:18 +0100

    add crumb certificate in add_folder script

--- scripts_jenkins/folders_management/add_folder.sh
+++ scripts_jenkins/folders_management/add_folder.sh
@@ -4,5 +4,9 @@ JOB_NAME=$2
 USER=$3
 PASS=$4
 
-curl -XPOST "${JENKINS_ROOT_URL}/createItem?name=${JOB_NAME}&mode=com.cloudbees.hudson.plugins.folder.Folder&from=&json={"name":"${JOB_NAME}","mode":"com.cloudbees.hudson.plugins.folder.Folder","from":"","Submit":"OK"}&Submit=OK" --user ${USER}:${PASS} -H "Content-Type:application/x-www-form-urlencoded"
+#Enable this code if the 'Prevent Cross Site Request Forgery exploits' option is enabled in Jenkins
+#CRUMB=$(curl --user $USER:$PASS \
+#    $JENKINS_ROOT_URL/crumbIssuer/api/xml?xpath=concat\(//crumbRequestField,%22:%22,//crumb\))
+#curl -X POST "${JENKINS_ROOT_URL}/createItem?name=${JOB_NAME}&mode=com.cloudbees.hudson.plugins.folder.Folder&from=&json={"name":"${JOB_NAME}","mode":"com.cloudbees.hudson.plugins.folder.Folder","from":"","Submit":"OK"}&Submit=OK" --user ${USER}:${PASS} -H "${CRUMB}" -H "Content-Type:application/x-www-form-urlencoded"
 
+curl -XPOST "${JENKINS_ROOT_URL}/createItem?name=${JOB_NAME}&mode=com.cloudbees.hudson.plugins.folder.Folder&from=&json={"name":"${JOB_NAME}","mode":"com.cloudbees.hudson.plugins.folder.Folder","from":"","Submit":"OK"}&Submit=OK" --user ${USER}:${PASS} -H "Content-Type:application/x-www-form-urlencoded"
