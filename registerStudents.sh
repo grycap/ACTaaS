f7cf57f54dbbf0df2f1891b4fab533f35ac8b4c6
amcaar <amcaar@i3m.upv.es>
2019-01-16 16:55:06 +0100

    Support CSRF security of Jenkins in registerStudents script and add a more friendly CLI

--- registerStudents.sh
+++ registerStudents.sh
@@ -1,49 +1,120 @@
 #!/bin/bash
 
-#File is a list of student's github accounts, one per line
-FILE=$1
-JENKINS_URL=$2
-PRACTICES=$3
-USER=$4
-PASS=$5
+usage() { echo "Usage: $0 [-f <students_names_file>] [-j <Jenkins_URL>] [-a <number_of_asssingments>] [-u <Jenkins_user>] [-p <Jenkins_password>] [-c <use_crumb_cred>]" 1>&2; exit 1; }
+
+CRUMB=false
 ROLE_NAME='alumno'
 
-#Create global role 'alumno'
-curl --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/addRole --data "type=globalRoles&roleName=${ROLE_NAME}&permissionIds=hudson.model.Hudson.Read,hudson.model.Item.Build&overwrite=true"
+while getopts ":f:j:a:u:p:c:" o; do
+    case "${o}" in
+        f)
+            #File is a list of student's github accounts, one per line
+            FILE=${OPTARG}
+            ;;
+        j)
+            JENKINS_URL=${OPTARG}
+            ;;
+        a)
+            PRACTICES=${OPTARG}
+            ;;
+        u)
+            USER=${OPTARG}
+            ;;
+        p)
+            PASS=${OPTARG}
+            ;;
+        c)
+            CRUMB=true
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
 
-#Read file and for each line, create the user, folders and project roles
+if [ -z "${FILE}" ] || [ -z "${JENKINS_URL}" ] || [ -z "${PRACTICES}" ] || [ -z "${USER}" ] || [ -z "${PASS}" ]; then
+    echo "ERROR: some parameters are missing, please consider usage."
+    usage
+fi
 
-while IFS='' read -r STUDENT || [[ -n "$STUDENT" ]]; do
-    #Create user
-    NEW_PASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-    #devolver al profesor la lista de alumnos y su contraseña de jenkins creada de forma aleatoria
-    echo "${STUDENT},${NEW_PASS}" >> students_jenkins_accounts.txt
-    CMD="jenkins.model.Jenkins.instance.securityRealm.createAccount(\"${STUDENT}\",\"${NEW_PASS}\")"
-    echo $CMD | java -jar scripts_jenkins/jenkins-cli.jar -auth ${USER}:${PASS} -s ${JENKINS_URL} groovy = 
-    #>/dev/null
+#Consider first if we have to use CSRF authentication
+if [ "$CRUMB" = true ] ; then
+    CRUMB_KEY=$(curl --user $USER:$PASS \
+        $JENKINS_URL/crumbIssuer/api/xml?xpath=concat\(//crumbRequestField,%22:%22,//crumb\))
+    #Create global role 'alumno'
+    curl --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/addRole --data "type=globalRoles&roleName=${ROLE_NAME}&permissionIds=hudson.model.Hudson.Read,hudson.model.Item.Build&overwrite=true" -H "${CRUMB_KEY}"
 
-    #Create the main folder
-    curl -X POST "${JENKINS_URL}/createItem?name=${STUDENT}&mode=com.cloudbees.hudson.plugins.folder.Folder&from=&json={"name":"${STUDENT}","mode":"com.cloudbees.hudson.plugins.folder.Folder","from":"","Submit":"OK"}&Submit=OK" --user ${USER}:${PASS} -H "Content-Type:application/x-www-form-urlencoded"
-    
-    #Create the subfolders (We need to know how many practices are in the subject)
-    i="0"
-    while [ $i -lt $PRACTICES ]
-    do
-        i=$[$i+1]
-        curl -X POST "${JENKINS_URL}/job/${STUDENT}/createItem?name=practica${i}_${STUDENT}&mode=com.cloudbees.hudson.plugins.folder.Folder&Submit=OK" -H "Content-Type:application/x-www-form-urlencoded" --user ${USER}:${PASS} 
-    done
-    
-    #Create project roles for each student, to restrict the view of the folders
-    curl --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/addRole --data "type=projectRoles&roleName=${STUDENT}&pattern=${STUDENT}&permissionIds=hudson.model.Item.Read,hudson.model.Item.Discover&overwrite=true"
-    curl --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/addRole --data "type=projectRoles&roleName=${STUDENT}sub&pattern=${STUDENT}/.*&permissionIds=hudson.model.Item.Read,hudson.model.Item.Discover&overwrite=true"
-    
-    #Assing global and project roles to the student
-    curl --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/assignRole --data "type=globalRoles&roleName=${ROLE_NAME}&sid=${STUDENT}"
-    curl --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/assignRole --data "type=projectRoles&roleName=${STUDENT}&sid=${STUDENT}"
-    curl --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/assignRole --data "type=projectRoles&roleName=${STUDENT}sub&sid=${STUDENT}"
+    #Read file and for each line, create the user, folders and project roles
 
-done < ${FILE}
+    while IFS='' read -r STUDENT || [[ -n "$STUDENT" ]]; do
+        #Create user
+        NEW_PASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+        #devolver al profesor la lista de alumnos y su contraseña de jenkins creada de forma aleatoria
+        echo "${STUDENT},${NEW_PASS}" >> students_jenkins_accounts.txt
+        CMD="jenkins.model.Jenkins.instance.securityRealm.createAccount(\"${STUDENT}\",\"${NEW_PASS}\")"
+        echo $CMD | java -jar scripts_jenkins/jenkins-cli.jar -auth ${USER}:${PASS} -s ${JENKINS_URL} groovy = 
+        #>/dev/null
 
-echo 'All users properly created in Jenkins! The file \'students_jenkins_accounts.txt\' contains details of each account.'
+        #Create the main folder
+        curl -X POST "${JENKINS_URL}/createItem?name=${STUDENT}&mode=com.cloudbees.hudson.plugins.folder.Folder&from=&json={"name":"${STUDENT}","mode":"com.cloudbees.hudson.plugins.folder.Folder","from":"","Submit":"OK"}&Submit=OK" --user ${USER}:${PASS} -H "${CRUMB_KEY}" -H "Content-Type:application/x-www-form-urlencoded"
+
+        #Create the subfolders (We need to know how many practices are in the subject)
+        i="0"
+        while [ $i -lt $PRACTICES ]
+        do
+            i=$[$i+1]
+            curl -X POST "${JENKINS_URL}/job/${STUDENT}/createItem?name=practica${i}_${STUDENT}&mode=com.cloudbees.hudson.plugins.folder.Folder&Submit=OK" -H "${CRUMB_KEY}" -H "Content-Type:application/x-www-form-urlencoded" --user ${USER}:${PASS} 
+        done
+
+        #Create project roles for each student, to restrict the view of the folders
+        curl --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/addRole --data "type=projectRoles&roleName=${STUDENT}&pattern=${STUDENT}&permissionIds=hudson.model.Item.Read,hudson.model.Item.Discover&overwrite=true" -H "${CRUMB_KEY}"
+        curl --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/addRole --data "type=projectRoles&roleName=${STUDENT}sub&pattern=${STUDENT}/.*&permissionIds=hudson.model.Item.Read,hudson.model.Item.Discover&overwrite=true" -H "${CRUMB_KEY}"
+
+        #Assing global and project roles to the student
+        curl --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/assignRole --data "type=globalRoles&roleName=${ROLE_NAME}&sid=${STUDENT}" -H "${CRUMB_KEY}"
+        curl --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/assignRole --data "type=projectRoles&roleName=${STUDENT}&sid=${STUDENT}" -H "${CRUMB_KEY}"
+        curl --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/assignRole --data "type=projectRoles&roleName=${STUDENT}sub&sid=${STUDENT}" -H "${CRUMB_KEY}"
+
+    done < ${FILE}
+else
+    #Create global role 'alumno'
+    curl --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/addRole --data "type=globalRoles&roleName=${ROLE_NAME}&permissionIds=hudson.model.Hudson.Read,hudson.model.Item.Build&overwrite=true"
+
+    #Read file and for each line, create the user, folders and project roles
+
+    while IFS='' read -r STUDENT || [[ -n "$STUDENT" ]]; do
+        #Create user
+        NEW_PASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+        #devolver al profesor la lista de alumnos y su contraseña de jenkins creada de forma aleatoria
+        echo "${STUDENT},${NEW_PASS}" >> students_jenkins_accounts.txt
+        CMD="jenkins.model.Jenkins.instance.securityRealm.createAccount(\"${STUDENT}\",\"${NEW_PASS}\")"
+        echo $CMD | java -jar scripts_jenkins/jenkins-cli.jar -auth ${USER}:${PASS} -s ${JENKINS_URL} groovy = 
+        #>/dev/null
+
+        #Create the main folder
+        curl -X POST "${JENKINS_URL}/createItem?name=${STUDENT}&mode=com.cloudbees.hudson.plugins.folder.Folder&from=&json={"name":"${STUDENT}","mode":"com.cloudbees.hudson.plugins.folder.Folder","from":"","Submit":"OK"}&Submit=OK" --user ${USER}:${PASS} -H "Content-Type:application/x-www-form-urlencoded"
+
+        #Create the subfolders (We need to know how many practices are in the subject)
+        i="0"
+        while [ $i -lt $PRACTICES ]
+        do
+            i=$[$i+1]
+            curl -X POST "${JENKINS_URL}/job/${STUDENT}/createItem?name=practica${i}_${STUDENT}&mode=com.cloudbees.hudson.plugins.folder.Folder&Submit=OK" -H "Content-Type:application/x-www-form-urlencoded" --user ${USER}:${PASS} 
+        done
+
+        #Create project roles for each student, to restrict the view of the folders
+        curl --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/addRole --data "type=projectRoles&roleName=${STUDENT}&pattern=${STUDENT}&permissionIds=hudson.model.Item.Read,hudson.model.Item.Discover&overwrite=true"
+        curl --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/addRole --data "type=projectRoles&roleName=${STUDENT}sub&pattern=${STUDENT}/.*&permissionIds=hudson.model.Item.Read,hudson.model.Item.Discover&overwrite=true"
+
+        #Assing global and project roles to the student
+        curl --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/assignRole --data "type=globalRoles&roleName=${ROLE_NAME}&sid=${STUDENT}"
+        curl --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/assignRole --data "type=projectRoles&roleName=${STUDENT}&sid=${STUDENT}"
+        curl --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/assignRole --data "type=projectRoles&roleName=${STUDENT}sub&sid=${STUDENT}"
+
+    done < ${FILE}
+fi
+
+echo -e '\n All users properly created in Jenkins! The file "students_jenkins_accounts.txt" contains details of each account.'
 exit 0
 
