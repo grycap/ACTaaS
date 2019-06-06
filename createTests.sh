1482775756f472629ad36c5cfaf37549e82d12f1
Amanda Calatrava <acalatrava46@gmail.com>
2019-06-06 17:52:29 +0200

    Add silent option and colours to the messages

--- createTests.sh
+++ createTests.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+cecho(){
+    RED="\033[0;31m"
+    GREEN='\033[0;32m'
+    YELLOW='\033[1;33m'
+    # ... ADD MORE COLORS
+    NC='\033[0m' # No Color
+
+    printf "${!1}${2} ${NC}\n"
+}
+
 usage() { echo "Usage: $0 [-f <students_names_file>] [-j <Jenkins_URL>] [-a <number_of_the_corresponding_asssingment>] [-n <name_of_the_exercise>] [-t <template_of_the_exercise.xml>] [-u <Jenkins_user>] [-p <Jenkins_password>] [-c <use_crumb_cred>]" 1>&2; exit 1; }
 
 CRUMB=false
@@ -40,29 +50,30 @@ done
 shift $((OPTIND-1))
 
 if [ -z "${FILE}" ] || [ -z "${JENKINS_URL}" ] || [ -z "${PRACTICA}" ] || [ -z "${JOB_NAME}" ] || [ -z "${JOB_TEMPLATE}" ] || [ -z "${USER}" ] || [ -z "${PASS}" ]; then
-    echo "ERROR: some parameters are missing, please consider usage."
+    cecho "RED" "ERROR: some parameters are missing, please consider usage."
     usage
 fi
 
 
 #Consider first if we have to use CSRF authentication
 if [ "$CRUMB" = true ] ; then
-    CRUMB_KEY=$(curl --user $USER:$PASS \
+    CRUMB_KEY=$(curl -s --user $USER:$PASS \
         $JENKINS_URL/crumbIssuer/api/xml?xpath=concat\(//crumbRequestField,%22:%22,//crumb\))
     while IFS=',' read STUDENT GIT_URL; do 
         # Modify the job template of Jenkins to include the repo of each student
         cat ${JOB_TEMPLATE} | sed "s/#URL#/$(echo $GIT_URL | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/g" >> /tmp/${JOB_NAME}_${STUDENT}
         # Create job in Jenkins for each student
-        curl -X POST "${JENKINS_URL}/job/${STUDENT}/job/practica${PRACTICA}_${STUDENT}/createItem?name=${JOB_NAME}" --user ${USER}:${PASS} --data-binary "@/tmp/${JOB_NAME}_${STUDENT}" -H "Content-Type:text/xml" -H "${CRUMB_KEY}"
+        curl -s -X POST "${JENKINS_URL}/job/${STUDENT}/job/practica${PRACTICA}_${STUDENT}/createItem?name=${JOB_NAME}" --user ${USER}:${PASS} --data-binary "@/tmp/${JOB_NAME}_${STUDENT}" -H "Content-Type:text/xml" -H "${CRUMB_KEY}"
     done < ${FILE}
 else
     while IFS=',' read STUDENT GIT_URL; do 
         # Modify the job template of Jenkins to include the repo of each student
         cat ${JOB_TEMPLATE} | sed "s/#URL#/$(echo $GIT_URL | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/g" >> /tmp/${JOB_NAME}_${STUDENT}
         # Create job in Jenkins for each student
-        curl -X POST "${JENKINS_URL}/job/${STUDENT}/job/practica${PRACTICA}_${STUDENT}/createItem?name=${JOB_NAME}" --user ${USER}:${PASS} --data-binary "@/tmp/${JOB_NAME}_${STUDENT}" -H "Content-Type:text/xml"
+        curl -s -X POST "${JENKINS_URL}/job/${STUDENT}/job/practica${PRACTICA}_${STUDENT}/createItem?name=${JOB_NAME}" --user ${USER}:${PASS} --data-binary "@/tmp/${JOB_NAME}_${STUDENT}" -H "Content-Type:text/xml"
     done < ${FILE}
 fi
 
-echo -e "\n Test for exercise ${JOB_NAME} properly created in Jenkins for each student!"
+#echo -e "\n Test for exercise ${JOB_NAME} properly created in Jenkins for each student!"
+cecho "GREEN" "\n Test for exercise ${JOB_NAME} properly created in Jenkins for each student!"
 exit 0
