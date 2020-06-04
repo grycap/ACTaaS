91603933c81020c7aebd7b7f1837596c6f7cfd55
marramon <61043057+marramon@users.noreply.github.com>
2020-06-04 17:08:01 +0200

    Add funtion to create single student

--- createTests.sh
+++ createTests.sh
@@ -10,17 +10,42 @@ cecho(){
     printf "${!1}${2} ${NC}\n"
 }
 
-usage() { echo "Usage: $0 [-f <students_names_file>] [-j <Jenkins_URL>] [-a <number_of_the_corresponding_asssingment>] [-n <name_of_the_exercise>] [-t <template_of_the_exercise.xml>] [-u <Jenkins_user>] [-p <Jenkins_password>] [-c <use_crumb_cred>]" 1>&2; exit 1; }
+usage() { 
+    echo "Usage: $0 [-f <students_names_file>] [-j <Jenkins_URL>] [-a <number_of_the_corresponding_asssingment>] [-n <name_of_the_exercise>] [-t <template_of_the_exercise.xml>] [-u <Jenkins_user>] [-p <Jenkins_password>] -s <student account> -g <github url> -i <credentials Id>" 1>&2; exit 1; 
+}
 
-CRUMB=false
+addstudent() {
+             EX="exercise"
+			 EX="$EX$JOB_NAME"
+             COM="sh /var/tmp/education/ACTaaS/practices/P${PRACTICA}/Face/unittests/run_test_exercise${JOB_NAME}.sh"
+
+			# Modify the job template of Jenkins to include the repo of each student
+             cat ${JOB_TEMPLATE} | sed "s/#URL#/$(echo $GIT_URL | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/g" > /tmp/${JOB_NAME}_${STUDENT}
+			 cat /tmp/${JOB_NAME}_${STUDENT} | sed "s/#EX#/$(echo $EX | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/g" > /tmp/${JOB_NAME}_${STUDENT}
+			 cat /tmp/${JOB_NAME}_${STUDENT} | sed "s/#COM#/$(echo $COM | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/g" > /tmp/${JOB_NAME}_${STUDENT}
+			 cat /tmp/${JOB_NAME}_${STUDENT} | sed "s/#ASIG#/$(echo $PRACTICA | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/g" > /tmp/${JOB_NAME}_${STUDENT}
+			 cat /tmp/${JOB_NAME}_${STUDENT} | sed "s/#CID#/$(echo $CID | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/g" > /tmp/${JOB_NAME}_${STUDENT}
+             
+			 # Create job in Jenkins for each student
+             curl -s -X POST "${JENKINS_URL}/job/${STUDENT}/job/practice${PRACTICA}_${STUDENT}/createItem?name=${JOB_NAME}" --user ${USER}:${PASS} --data-binary "@/tmp/${JOB_NAME}_${STUDENT}" -H "Content-Type:text/xml"
+ }
+
+readfile() {
+
+    while IFS=',' read STUDENT GIT_URL; do 
+         addstudent
+   done < ${FILE}
+     
+}
+
 ROLE_NAME='alumno'
 
-while getopts ":f:j:a:n:t:u:p:c:" o; do
+while getopts ":f:j:a:n:t:u:p:s:g:i:" o; do
     case "${o}" in
         f)
             #File is a combination of 'student_git_account,git_url', one per line
             FILE=${OPTARG}
-            ;;
+			;;
         j)
             JENKINS_URL=${OPTARG}
             ;;
@@ -29,7 +54,7 @@ while getopts ":f:j:a:n:t:u:p:c:" o; do
             ;;
         n)
             JOB_NAME=${OPTARG}
-            ;;
+		    ;;
         t)
             JOB_TEMPLATE=${OPTARG}
             ;;
@@ -39,41 +64,34 @@ while getopts ":f:j:a:n:t:u:p:c:" o; do
         p)
             PASS=${OPTARG}
             ;;
-        c)
-            CRUMB=true
+        s)
+            STUDENT=${OPTARG}
+			;;
+		g)
+            GIT_URL=${OPTARG}
             ;;
-        *)
+		i)
+            CID=${OPTARG}
+            ;;
+       *)
             usage
             ;;
     esac
 done
 shift $((OPTIND-1))
 
-if [ -z "${FILE}" ] || [ -z "${JENKINS_URL}" ] || [ -z "${PRACTICA}" ] || [ -z "${JOB_NAME}" ] || [ -z "${JOB_TEMPLATE}" ] || [ -z "${USER}" ] || [ -z "${PASS}" ]; then
+if [ -z "${JENKINS_URL}" ] || [ -z "${CID}" ] || [ -z "${PRACTICA}" ] || [ -z "${JOB_NAME}" ] || [ -z "${JOB_TEMPLATE}" ] || [ -z "${USER}" ] || [ -z "${PASS}" ]; then
     cecho "RED" "ERROR: some parameters are missing, please consider usage."
     usage
+    exit
 fi
 
-
-#Consider first if we have to use CSRF authentication
-if [ "$CRUMB" = true ] ; then
-    CRUMB_KEY=$(curl -s --user $USER:$PASS \
-        $JENKINS_URL/crumbIssuer/api/xml?xpath=concat\(//crumbRequestField,%22:%22,//crumb\))
-    while IFS=',' read STUDENT GIT_URL; do 
-        # Modify the job template of Jenkins to include the repo of each student
-        cat ${JOB_TEMPLATE} | sed "s/#URL#/$(echo $GIT_URL | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/g" >> /tmp/${JOB_NAME}_${STUDENT}
-        # Create job in Jenkins for each student
-        curl -s -X POST "${JENKINS_URL}/job/${STUDENT}/job/practica${PRACTICA}_${STUDENT}/createItem?name=${JOB_NAME}" --user ${USER}:${PASS} --data-binary "@/tmp/${JOB_NAME}_${STUDENT}" -H "Content-Type:text/xml" -H "${CRUMB_KEY}"
-    done < ${FILE}
-else
-    while IFS=',' read STUDENT GIT_URL; do 
-        # Modify the job template of Jenkins to include the repo of each student
-        cat ${JOB_TEMPLATE} | sed "s/#URL#/$(echo $GIT_URL | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/g" >> /tmp/${JOB_NAME}_${STUDENT}
-        # Create job in Jenkins for each student
-        curl -s -X POST "${JENKINS_URL}/job/${STUDENT}/job/practica${PRACTICA}_${STUDENT}/createItem?name=${JOB_NAME}" --user ${USER}:${PASS} --data-binary "@/tmp/${JOB_NAME}_${STUDENT}" -H "Content-Type:text/xml"
-    done < ${FILE}
+if [ -z "${FILE}" ]; then
+     addstudent
+    else
+	 readfile
 fi
 
-#echo -e "\n Test for exercise ${JOB_NAME} properly created in Jenkins for each student!"
 cecho "GREEN" "\n Test for exercise ${JOB_NAME} properly created in Jenkins for each student!"
-exit 0
+
+
--- registerStudents.sh
+++ registerStudents.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ROLE_NAME='alumno'
+
 cecho(){
     RED="\033[0;31m"
     GREEN='\033[0;32m'
@@ -10,16 +12,54 @@ cecho(){
     printf "${!1}${2} ${NC}\n"
 }
 
-usage() { echo "Usage: $0 [-f <students_names_file>] [-j <Jenkins_URL>] [-a <number_of_asssingments>] [-u <Jenkins_user>] [-p <Jenkins_password>] [-c <use_crumb_cred>]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-f <students_names_file>] [-j <Jenkins_URL>] [-a <number_of_asssingments>] [-u <Jenkins_user>] [-p <Jenkins_password>] [-s <single_student_account>]" 1>&2; exit 1; }
 
-CRUMB=false
-ROLE_NAME='alumno'
+createStudent() {
 
-while getopts ":f:j:a:u:p:c:" o; do
+	#Create user
+	#NEW_PASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+	NEW_PASS="vamosacambiarla"        
+	#devolver al profesor la lista de alumnos y su contraseña de jenkins creada de forma aleatoria
+	echo "${STUDENT},${NEW_PASS}" >> students_jenkins_accounts.txt
+	CMD="jenkins.model.Jenkins.instance.securityRealm.createAccount(\"${STUDENT}\",\"${NEW_PASS}\")"
+	echo $CMD | java -jar jenkins-cli.jar -auth ${USER}:${PASS} -s ${JENKINS_URL} groovy = 
+	#>/dev/null
+
+	#Create the main folder
+	curl -s -X POST "${JENKINS_URL}/createItem?name=${STUDENT}&mode=com.cloudbees.hudson.plugins.folder.Folder&from=&json={"name":"${STUDENT}","mode":"com.cloudbees.hudson.plugins.folder.Folder","from":"","Submit":"OK"}&Submit=OK" --user ${USER}:${PASS} -H "Content-Type:application/x-www-form-urlencoded"
+
+	#Create the subfolders (We need to know how many practices are in the subject)
+	i="0"
+	while [ $i -lt $PRACTICES ]
+	do
+       curl -s -X POST "${JENKINS_URL}/job/${STUDENT}/createItem?name=practice${i}_${STUDENT}&mode=com.cloudbees.hudson.plugins.folder.Folder&Submit=OK" -H "Content-Type:application/x-www-form-urlencoded" --user ${USER}:${PASS} 
+       i=$[$i+1]
+    done
+
+        #Create project roles for each student, to restrict the view of the folders
+        curl -s --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/addRole --data "type=projectRoles&roleName=${STUDENT}&pattern=${STUDENT}&permissionIds=hudson.model.Item.Read,hudson.model.Item.Discover&overwrite=true"
+        curl -s --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/addRole --data "type=projectRoles&roleName=${STUDENT}sub&pattern=${STUDENT}/.*&permissionIds=hudson.model.Item.Read,hudson.model.Item.Discover&overwrite=true"
+
+        #Assing global and project roles to the student
+        curl -s --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/assignRole --data "type=globalRoles&roleName=${ROLE_NAME}&sid=${STUDENT}"
+        curl -s --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/assignRole --data "type=projectRoles&roleName=${STUDENT}&sid=${STUDENT}"
+        curl -s --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/assignRole --data "type=projectRoles&roleName=${STUDENT}sub&sid=${STUDENT}"
+}
+
+readFile() {
+	while IFS='' read -r STUDENT || [[ -n "$STUDENT" ]]; 
+	do
+	  createStudent
+    done < ${FILE}
+	
+}
+
+while getopts ":f:j:a:u:s:p:" o; do
     case "${o}" in
         f)
             #File is a list of student's github accounts, one per line
             FILE=${OPTARG}
+			
             ;;
         j)
             JENKINS_URL=${OPTARG}
@@ -32,9 +72,9 @@ while getopts ":f:j:a:u:p:c:" o; do
             ;;
         p)
             PASS=${OPTARG}
-            ;;
-        c)
-            CRUMB=true
+           ;;
+       s)
+            STUDENT=${OPTARG}
             ;;
         *)
             usage
@@ -43,91 +83,28 @@ while getopts ":f:j:a:u:p:c:" o; do
 done
 shift $((OPTIND-1))
 
-if [ -z "${FILE}" ] || [ -z "${JENKINS_URL}" ] || [ -z "${PRACTICES}" ] || [ -z "${USER}" ] || [ -z "${PASS}" ]; then
+# API token actual
+# PASS="111bdf145a6545c45940ae73cdc6dd72ea" 
+
+if [ -z "${JENKINS_URL}" ] || [ -z "${PRACTICES}" ] || [ -z "${USER}" ] || [ -z "${PASS}" ]; then
     cecho "RED" "ERROR: some parameters are missing, please consider usage."
     usage
+     exit
 fi
 
-#Consider first if we have to use CSRF authentication
-if [ "$CRUMB" = true ] ; then
-    CRUMB_KEY=$(curl -s --user $USER:$PASS \
-        $JENKINS_URL/crumbIssuer/api/xml?xpath=concat\(//crumbRequestField,%22:%22,//crumb\))
-    #Create global role 'alumno'
-    curl -s --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/addRole --data "type=globalRoles&roleName=${ROLE_NAME}&permissionIds=hudson.model.Hudson.Read,hudson.model.Item.Build&overwrite=true" -H "${CRUMB_KEY}"
+if [ ! -f "jenkins-cli.jar" ]; then
+    cecho "RED" "ERROR: file jenkins-cli.jar not found."
+	exit
+fi
 
-    #Read file and for each line, create the user, folders and project roles
+#Create global role 'alumno'
+curl -s --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/addRole --data "type=globalRoles&roleName=${ROLE_NAME}&permissionIds=hudson.model.Hudson.Read,hudson.model.Item.Build&overwrite=true"
 
-    while IFS='' read -r STUDENT || [[ -n "$STUDENT" ]]; do
-        #Create user
-        #NEW_PASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-        NEW_PASS="vamosacambiarla"
-        #devolver al profesor la lista de alumnos y su contraseña de jenkins creada de forma aleatoria
-        echo "${STUDENT},${NEW_PASS}" >> students_jenkins_accounts.txt
-        CMD="jenkins.model.Jenkins.instance.securityRealm.createAccount(\"${STUDENT}\",\"${NEW_PASS}\")"
-        echo $CMD | java -jar scripts_jenkins/jenkins-cli.jar -auth ${USER}:${PASS} -s ${JENKINS_URL} groovy = 
-        #>/dev/null
 
-        #Create the main folder
-        curl -s -X POST "${JENKINS_URL}/createItem?name=${STUDENT}&mode=com.cloudbees.hudson.plugins.folder.Folder&from=&json={"name":"${STUDENT}","mode":"com.cloudbees.hudson.plugins.folder.Folder","from":"","Submit":"OK"}&Submit=OK" --user ${USER}:${PASS} -H "${CRUMB_KEY}" -H "Content-Type:application/x-www-form-urlencoded"
-
-        #Create the subfolders (We need to know how many practices are in the subject)
-        i="0"
-        while [ $i -lt $PRACTICES ]
-        do
-            i=$[$i+1]
-            curl -s -X POST "${JENKINS_URL}/job/${STUDENT}/createItem?name=practice${i}_${STUDENT}&mode=com.cloudbees.hudson.plugins.folder.Folder&Submit=OK" -H "${CRUMB_KEY}" -H "Content-Type:application/x-www-form-urlencoded" --user ${USER}:${PASS} 
-        done
-
-        #Create project roles for each student, to restrict the view of the folders
-        curl -s --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/addRole --data "type=projectRoles&roleName=${STUDENT}&pattern=${STUDENT}&permissionIds=hudson.model.Item.Read,hudson.model.Item.Discover&overwrite=true" -H "${CRUMB_KEY}"
-        curl -s --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/addRole --data "type=projectRoles&roleName=${STUDENT}sub&pattern=${STUDENT}/.*&permissionIds=hudson.model.Item.Read,hudson.model.Item.Discover&overwrite=true" -H "${CRUMB_KEY}"
-
-        #Assing global and project roles to the student
-        curl -s --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/assignRole --data "type=globalRoles&roleName=${ROLE_NAME}&sid=${STUDENT}" -H "${CRUMB_KEY}"
-        curl -s --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/assignRole --data "type=projectRoles&roleName=${STUDENT}&sid=${STUDENT}" -H "${CRUMB_KEY}"
-        curl -s --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/assignRole --data "type=projectRoles&roleName=${STUDENT}sub&sid=${STUDENT}" -H "${CRUMB_KEY}"
-
-    done < ${FILE}
+if [ -z "${FILE}" ]
+then
+   createStudent
 else
-    #Create global role 'alumno'
-    curl -s --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/addRole --data "type=globalRoles&roleName=${ROLE_NAME}&permissionIds=hudson.model.Hudson.Read,hudson.model.Item.Build&overwrite=true"
-
-    #Read file and for each line, create the user, folders and project roles
-
-    while IFS='' read -r STUDENT || [[ -n "$STUDENT" ]]; do
-        #Create user
-        #NEW_PASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-        NEW_PASS="vamosacambiarla"        
-        #devolver al profesor la lista de alumnos y su contraseña de jenkins creada de forma aleatoria
-        echo "${STUDENT},${NEW_PASS}" >> students_jenkins_accounts.txt
-        CMD="jenkins.model.Jenkins.instance.securityRealm.createAccount(\"${STUDENT}\",\"${NEW_PASS}\")"
-        echo $CMD | java -jar scripts_jenkins/jenkins-cli.jar -auth ${USER}:${PASS} -s ${JENKINS_URL} groovy = 
-        #>/dev/null
-
-        #Create the main folder
-        curl -s -X POST "${JENKINS_URL}/createItem?name=${STUDENT}&mode=com.cloudbees.hudson.plugins.folder.Folder&from=&json={"name":"${STUDENT}","mode":"com.cloudbees.hudson.plugins.folder.Folder","from":"","Submit":"OK"}&Submit=OK" --user ${USER}:${PASS} -H "Content-Type:application/x-www-form-urlencoded"
-
-        #Create the subfolders (We need to know how many practices are in the subject)
-        i="0"
-        while [ $i -lt $PRACTICES ]
-        do
-            curl -s -X POST "${JENKINS_URL}/job/${STUDENT}/createItem?name=practice${i}_${STUDENT}&mode=com.cloudbees.hudson.plugins.folder.Folder&Submit=OK" -H "Content-Type:application/x-www-form-urlencoded" --user ${USER}:${PASS} 
-            i=$[$i+1]
-        done
-
-        #Create project roles for each student, to restrict the view of the folders
-        curl -s --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/addRole --data "type=projectRoles&roleName=${STUDENT}&pattern=${STUDENT}&permissionIds=hudson.model.Item.Read,hudson.model.Item.Discover&overwrite=true"
-        curl -s --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/addRole --data "type=projectRoles&roleName=${STUDENT}sub&pattern=${STUDENT}/.*&permissionIds=hudson.model.Item.Read,hudson.model.Item.Discover&overwrite=true"
-
-        #Assing global and project roles to the student
-        curl -s --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/assignRole --data "type=globalRoles&roleName=${ROLE_NAME}&sid=${STUDENT}"
-        curl -s --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/assignRole --data "type=projectRoles&roleName=${STUDENT}&sid=${STUDENT}"
-        curl -s --user ${USER}:${PASS} ${JENKINS_URL}/role-strategy/strategy/assignRole --data "type=projectRoles&roleName=${STUDENT}sub&sid=${STUDENT}"
-
-    done < ${FILE}
-fi
-
-#echo -e '\n All users properly created in Jenkins! The file "students_jenkins_accounts.txt" contains details of each account.'
-cecho "GREEN" '\n All users properly created in Jenkins! The file "students_jenkins_accounts.txt" contains details of each account.'
-exit 0
-
+    readFile
+fi   
+    
