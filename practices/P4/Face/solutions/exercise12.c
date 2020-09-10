5942eb7cd13229ba98d22564256309a07b3f059f
marramon <61043057+marramon@users.noreply.github.com>
2020-09-10 03:11:40 +0200

    Version 0

---
+++ practices/P4/Face/solutions/exercise12.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+
+	int n, l, i, j, k, sp;
+
+	do
+    {
+        printf("Enter the number of asterisks per line: ");
+        scanf("%d", &n);
+    } while(n < 1);
+
+	do
+    {
+        printf("Enter the number of lines: ");
+        scanf("%d", &l);
+    } while(n < 1);
+
+    sp = l;
+    for(i = 1; i <= l; i++)
+    {
+      for(k = 1; k <= sp; k++)
+          putchar(' ');
+      for(j = 1; j <= n; j++)
+         putchar('*');
+
+      putchar('\n');
+      sp--;
+    }
+
+    return 0;
+}
