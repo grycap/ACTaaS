c5730956eefce2320bef7a351ecf898e89d6044a
marramon <61043057+marramon@users.noreply.github.com>
2020-10-14 21:31:49 +0200

    Update test_exercise6.sh

--- practices/P1/Autonomous/unittests/test_exercise6.sh
+++ practices/P1/Autonomous/unittests/test_exercise6.sh
@@ -8,15 +8,17 @@ fi
 ch=$1
 
 asc=$(echo $ch | awk 'BEGIN{for(n=0;n<256;n++)ord[sprintf("%c",n)]=n}{print ord[$1]}')
+ant=$(echo "$asc - 1"|bc)
+ant=$(echo $ant | awk 'BEGIN{for(n=0;n<256;n++)chr[n]=sprintf("%c",n)}{print chr[$1]}')
 next=$(echo "$asc + 1"|bc)
 next=$(echo $next | awk 'BEGIN{for(n=0;n<256;n++)chr[n]=sprintf("%c",n)}{print chr[$1]}')
 echo "$@" > ent.txt
 ./exercise6_bin < ent.txt > sal.txt
-sal=$(grep -oE $next sal.txt)
-
+ant_alu=$(grep -oE $ant sal.txt)
+next_alu=$(grep -oE $next sal.txt)
 rm ent.txt sal.txt
 
-if [ "$next" = "$sal" ]; then
+if [ "$ant" = "$ant_alu" ] && [ "$next" = "$next_alu" ]; then
    echo "Test OK!!"
    exit 0
 else
