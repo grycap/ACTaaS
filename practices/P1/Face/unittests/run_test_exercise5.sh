6bd906d098c9dbe4e3dd01ea1f4101965132003e
marramon <61043057+marramon@users.noreply.github.com>
2020-10-16 01:37:17 +0200

    updated version

--- practices/P1/Face/unittests/run_test_exercise5.sh
+++ practices/P1/Face/unittests/run_test_exercise5.sh
@@ -1,7 +1,54 @@
-sh /var/tmp/education/ACTaaS/practices/P1/Face/unittests/test_exercise5.sh 137
-if [ $? -eq 1 ]   
+#!/bin/bash
+
+sh /var/tmp/education/ACTaaS/practices/P1/Face/unittests/test_exercise5.sh 0
+if [ $? -eq 1 ]
+then
+   exit 1
+fi
+
+
+sh /var/tmp/education/ACTaaS/practices/P1/Face/unittests/test_exercise5.sh 300
+if [ $? -eq 1 ]
+then
+   exit 1
+fi
+
+sh /var/tmp/education/ACTaaS/practices/P1/Face/unittests/test_exercise5.sh 60
+if [ $? -eq 1 ]
+then
+   exit 1
+fi
+
+sh /var/tmp/education/ACTaaS/practices/P1/Face/unittests/test_exercise5.sh 15
+if [ $? -eq 1 ]
 then
   exit 1
-else
-  exit 0 
 fi
+
+sh /var/tmp/education/ACTaaS/practices/P1/Face/unittests/test_exercise5.sh 12
+if [ $? -eq 1 ]
+then
+  exit 1
+fi
+
+sh /var/tmp/education/ACTaaS/practices/P1/Face/unittests/test_exercise5.sh 7
+if [ $? -eq 1 ]
+then
+  exit 1
+fi
+
+sh /var/tmp/education/ACTaaS/practices/P1/Face/unittests/test_exercise5.sh 4
+if [ $? -eq 1 ]
+then
+  exit 1
+fi
+sh /var/tmp/education/ACTaaS/practices/P1/Face/unittests/test_exercise5.sh 137
+if [ $? -eq 1 ]
+then
+  exit 1
+fi
+
+exit 0
+
+
+
--- practices/P1/Face/unittests/test_exercise5.sh
+++ practices/P1/Face/unittests/test_exercise5.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ $# -ne 1 ]; then
-   echo "test5 <number of agendas>"
+   echo "$0 <number of agendas>"
    exit
 fi
 
@@ -18,17 +18,24 @@ aux=$(echo "$aux % $MEDIUM_BOX"|bc)
 n_small_boxes=$(echo "$aux / $SMALL_BOX"|bc)
 aux=$(echo "$aux % $SMALL_BOX" | bc)
 
-ent=$(printf '%d BIG BOXES -- %d MEDIUM BOXES -- %d SMALL BOXES -- %d Individual agendas' $n_big_boxes $n_medium_boxes $n_small_boxes $aux| tr -d -c 0-9)
-
+ent=$(printf '%d BIG BOXES -- %d MEDIUM BOXES -- %d SMALL BOXES -- %d Individual agendas.' $n_big_boxes $n_medium_boxes $n_small_boxes $aux)
+values=$(echo "$ent"| sed 's/[^0-9]*//g' )
 echo "$@" > ent.txt
 ./exercise5_bin < ent.txt > sal.txt
-sal=$(grep  "BIG BOXES --" sal.txt| tr -d -c 0-9)
+
+sal=$(cat sal.txt)
+pos=$(echo `expr index "$sal" 'B'`)
+pos=$(echo "$pos - 2"|bc)
+sal=$(cat sal.txt| cut -c $pos-)
+values_alu=$(echo "$sal"| sed 's/[^0-9]*//g' )
 rm ent.txt sal.txt
-if [ "$ent" = "$sal" ]; then
+if [ $values -eq $values_alu ]; then
    echo "Test OK!!"
    exit 0
 else
   echo "Test ERROR!!"
- exit 1
+  echo "For input = $n_agendas agendas"
+  echo "Spected sal: $ent"
+  echo "Student output: $sal"
+  exit 1
 fi
-
