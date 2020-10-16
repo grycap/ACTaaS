4295c37d078a8afcc35137f776ff608cb6b70760
marramon <61043057+marramon@users.noreply.github.com>
2020-10-16 11:58:38 +0200

    Update test_exercise2.sh

--- practices/P1/Autonomous/unittests/test_exercise2.sh
+++ practices/P1/Autonomous/unittests/test_exercise2.sh
@@ -9,15 +9,13 @@ uppercase=$(echo "$1" | tr a-z A-Z)
 
 echo "$@" > ent.txt
 ./exercise2_bin < ent.txt > sal.txt
-
-sal=$(awk 'END {print $NF}' sal.txt)
-rm ent.txt sal.txt
+cat sal.txt | sed -e "s/[[:punct:]]\+//g"|tr -d '\n' > output.txt
+sal=$(awk 'END {print $NF}' output.txt)
+rm ent.txt sal.txt output.txt
 
 if [ "$uppercase" = "$sal" ]; then
    echo "Test OK!!"
-   exit 0
 else
   echo "Test ERROR!!"
-  exit 1
 fi
 
