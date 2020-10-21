3c6b5c71f64775463af2249aa185ec0d548abb7b
marramon <61043057+marramon@users.noreply.github.com>
2020-10-22 01:31:25 +0200

    Update test_exercise2.sh

--- practices/P2/Face/unittests/test_exercise2.sh
+++ practices/P2/Face/unittests/test_exercise2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ ! -f input.txt ]; then
+if [ ! -f ./input.txt ]; then
     echo "Test ERROR -- File input.txt found!"
     exit 1
 fi
 
-n=$(cat input.txt)
+n=$(cat ./input.txt)
 square=$(echo "$n * $n"|bc)
 ./exercise2_bin
 m=$(cat output.txt)
