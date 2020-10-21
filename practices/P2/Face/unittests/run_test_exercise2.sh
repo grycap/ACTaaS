aac8894e43a7a5a4336caa99dd7fe3e355773e75
marramon <61043057+marramon@users.noreply.github.com>
2020-10-22 01:59:39 +0200

    updated version

--- practices/P2/Face/unittests/run_test_exercise2.sh
+++ practices/P2/Face/unittests/run_test_exercise2.sh
@@ -1,5 +1,7 @@
 sh /var/tmp/education/ACTaaS/practices/P2/Face/unittests/test_exercise2.sh
-
+cd exercise2
+cp *.txt ../
+cd..
 if [ $? -eq 1 ]   
 then
   exit 1
--- practices/P2/Face/unittests/test_exercise2.sh
+++ practices/P2/Face/unittests/test_exercise2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ ! -f ./exercise2/input.txt ]; then
+if [ ! -f input.txt ]; then
     echo "Test ERROR -- File input.txt found!"
     exit 1
 fi
 
-n=$(cat ./exercise2/input.txt)
+n=$(cat input.txt)
 square=$(echo "$n * $n"|bc)
 ./exercise2_bin
 m=$(cat output.txt)
