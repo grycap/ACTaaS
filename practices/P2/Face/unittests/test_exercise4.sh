032624b221428e9f6604861f3f16e130d5e2280f
marramon <61043057+marramon@users.noreply.github.com>
2020-10-22 04:17:05 +0200

    Update test_exercise4.sh

--- practices/P2/Face/unittests/test_exercise4.sh
+++ practices/P2/Face/unittests/test_exercise4.sh
@@ -5,11 +5,14 @@ if [ $# -ne 2 ]; then
    exit
 fi
 
-profit=$(echo "$1 * ($2 / 100)"|bc -l)
+if [ ! -f price_margin.txt ]; then
+    echo "Test ERROR -- File price_margin.txt found!"
+    exit 1
+fi
+
+profit=$(echo "$1 * ($2 / 100.0)"|bc -l)
 sp=$(echo "$1 + $profit" | bc -l)
-
-ent=$(printf "sellingprice %.2f" $sp)
-
+sp=$(printf "%.2f" $sp)
 for i in "$@"
 do
   echo "$i" >> ent.txt
@@ -17,18 +20,11 @@ done
 
 ./exercise4_bin
 
-sal=$(grep -oE $sp price_margin.txt)
-sal=$(printf "%.2f" $sal)
-if [ "$ent" = "$sal" ]; then
+sal=$(grep -oE $sp price_margin.txt|tail -n1)
+if [ "$sp" = "$sal" ]; then
    echo "Test OK!!"
-   rm ent.txt price_margin.txt
-   exit 0
 else
    echo "Test ERROR!!"
-   echo "EXPECTED OUTPUT: $ent"
-   echo "STUDENT OUTPUT: $sal"
-   rm ent.txt price_margin.txt
-   exit 1
 fi
 
-
+rm *.txt
