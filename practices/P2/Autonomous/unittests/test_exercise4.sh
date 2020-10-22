72a44bbddf30a3457ae81831fbe3cd8ffe739333
marramon <61043057+marramon@users.noreply.github.com>
2020-10-22 08:21:07 +0200

    Update test_exercise4.sh

--- practices/P2/Autonomous/unittests/test_exercise4.sh
+++ practices/P2/Autonomous/unittests/test_exercise4.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 ./exercise4_bin < text.dat > ex4.dat
 awk 'NF' ex4.dat > ex4.bak
-mv ex4.bak ex4.dat
-if cmp -s text.dat /var/tmp/education/ACTaaS/practices/P2/Autonomous/unittests/ex4_sol.dat; then
+mv ex4.bak ex4_sol.dat
+if cmp -s text.dat ex4_sol.dat; then
     echo "Test OK!!"
+    rm text.dat ex4_sol.dat
     exit 0
 else
     echo "Test ERROR -- text.dat is not the same as exercise 3"
+    rm text.dat ex4_sol.dat 
     exit 1
 fi
 
