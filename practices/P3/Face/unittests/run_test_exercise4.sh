540b5fe1a59bafae8988f2c3567807a24e1ffc99
marramon <61043057+marramon@users.noreply.github.com>
2020-10-23 19:45:06 +0200

    Update run_test_exercise4.sh

--- practices/P3/Face/unittests/run_test_exercise4.sh
+++ practices/P3/Face/unittests/run_test_exercise4.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+cp ./exercise4/* user.txt
 sh /var/tmp/education/ACTaaS/practices/P3/Face/unittests/test_exercise4.sh ficha.txt "J. Damian Segrelles Quilis" 48285791M 14 59
 
 if [ $? -eq 1 ]   
