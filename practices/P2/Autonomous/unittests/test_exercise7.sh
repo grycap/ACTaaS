90e5cba2775573dff2cd8502a7857c1a944666b7
marramon <61043057+marramon@users.noreply.github.com>
2020-10-23 03:52:47 +0200

    Update test_exercise7.sh

--- practices/P2/Autonomous/unittests/test_exercise7.sh
+++ practices/P2/Autonomous/unittests/test_exercise7.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-if [  -f ex7_sol.txt ]; then
+if [  -f ex7_alu.txt ]; then
    rm ex7_alu.txt
 fi
 ./exercise7_bin
+if [ ! -f ex7_sol.txt ]; then
+   echo "Test ERROR -- File ex7_alu.txt not found."
+   exit 1
+fi
+
 if cmp -s /var/tmp/education/ACTaaS/practices/P2/Autonomous/unittests/ex7_sol.txt ex7_alu.txt; then
     echo "Test OK!!"
     rm ex7_alu.txt
