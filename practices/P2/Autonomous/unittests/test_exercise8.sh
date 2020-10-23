cf950e5950fc77418565b990e8a96a278895babc
marramon <61043057+marramon@users.noreply.github.com>
2020-10-23 05:05:04 +0200

    Update test_exercise8.sh

--- practices/P2/Autonomous/unittests/test_exercise8.sh
+++ practices/P2/Autonomous/unittests/test_exercise8.sh
@@ -11,14 +11,14 @@ fi
 
 if cmp -s /var/tmp/education/ACTaaS/practices/P2/Autonomous/unittests/ex8_sol.txt ex7_alu.txt; then
     echo "Test OK!!"
-    rm ex7_alu.txt 
+    rm ex7_alu.txt exercise8.c 
     exit 0
 else
    echo "Test ERROR -- EXPECTED OUTPUT:"
    cat /var/tmp/education/ACTaaS/practices/P2/Autonomous/unittests/ex8_sol.txt
    echo "STUDENT OUTPUT:"
    cat ex7_alu.txt
-   rm ex7_alu.txt
+   rm ex7_alu.txt exercise8.c
    exit 1
 fi
 
