73c3dd55cb484d40b0cdf6c20dfb0bbe6fca23c3
marramon <61043057+marramon@users.noreply.github.com>
2020-10-23 17:26:41 +0200

    Update run_test_exercise1.sh

--- practices/P3/Face/unittests/run_test_exercise1.sh
+++ practices/P3/Face/unittests/run_test_exercise1.sh
@@ -1,8 +1,25 @@
+echo Tsting absolute value of negative integer.....
 sh /var/tmp/education/ACTaaS/practices/P3/Face/unittests/test_exercise1.sh -25
 
 if [ $? -eq 1 ]   
 then
   exit 1
-else
-  exit 0 
 fi
+
+echo Tsting absolute value of positive integer.....
+sh /var/tmp/education/ACTaaS/practices/P3/Face/unittests/test_exercise1.sh 123
+
+if [ $? -eq 1 ]   
+then
+  exit 1
+fi
+
+echo Tsting absolute value of number 0
+sh /var/tmp/education/ACTaaS/practices/P3/Face/unittests/test_exercise1.sh 0
+
+if [ $? -eq 1 ]   
+then
+  exit 1
+fi
+
+exit 0
