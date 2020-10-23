a700889d18488e454a518b108988698bd469d991
marramon <61043057+marramon@users.noreply.github.com>
2020-10-23 20:11:15 +0200

    Update run_test_exercise4.sh

--- practices/P3/Face/unittests/run_test_exercise4.sh
+++ practices/P3/Face/unittests/run_test_exercise4.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-cp ./exercise4/* user.txt
-sh /var/tmp/education/ACTaaS/practices/P3/Face/unittests/test_exercise4.sh /var/tmp/education/ACTaaS
-/ficha.txt "J. Damian Segrelles Quilis" 48285791M 14 59
+cp ./exercise4/user.txt ficha.txt .
+sh /var/tmp/education/ACTaaS/practices/P3/Face/unittests/test_exercise4.sh ficha.txt "J. Damian Segrelles Quilis" 48285791M 14 59
 
 if [ $? -eq 1 ]   
 then
