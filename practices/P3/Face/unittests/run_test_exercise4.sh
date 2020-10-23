d75e9ed68c0bd145c7888bed949f2c15eefd8828
marramon <61043057+marramon@users.noreply.github.com>
2020-10-23 21:23:45 +0200

    Update run_test_exercise4.sh

--- practices/P3/Face/unittests/run_test_exercise4.sh
+++ practices/P3/Face/unittests/run_test_exercise4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp ./exercise4/*.txt .
-echo "Testing age < 18 and credits > 30 case..."
+echo "Testing age < 18 and credits < 30 case..."
 sh /var/tmp/education/ACTaaS/practices/P3/Face/unittests/test_exercise4.sh ficha.txt "J. Damian Segrelles Quilis" 48285791M 14 59
 
 if [ $? -eq 1 ]   
