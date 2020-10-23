d37fd31cf98caf4a525a30523b55342a0bfc78a6
marramon <61043057+marramon@users.noreply.github.com>
2020-10-23 21:18:45 +0200

    updated versions

--- practices/P3/Face/unittests/run_test_exercise4.sh
+++ practices/P3/Face/unittests/run_test_exercise4.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 cp ./exercise4/*.txt .
-sh /var/tmp/education/ACTaaS/practices/P3/Face/unittests/test_exercise4.sh ficha.txt "J. Damian Segrelles Quilis" 48285791M 23 59
+echo "Testing age < 18 and credits > 30 case..."
+sh /var/tmp/education/ACTaaS/practices/P3/Face/unittests/test_exercise4.sh ficha.txt "J. Damian Segrelles Quilis" 48285791M 14 59
 
 if [ $? -eq 1 ]   
 then
   exit 1
-else
-  exit 0
 fi
+echo "Testing age > 18 and credits < 30 case..."
+sh /var/tmp/education/ACTaaS/practices/P3/Face/unittests/test_exercise4.sh ficha.txt "J. Damian Segrelles Quilis" 48285791M 21 14
+
+if [ $? -eq 1 ]   
+then
+  exit 1
+fi
+echo "Testing age > 18 and credits > 30 case..."
+sh /var/tmp/education/ACTaaS/practices/P3/Face/unittests/test_exercise4.sh ficha.txt "J. Damian Segrelles Quilis" 48285791M 80 34
+
+if [ $? -eq 1 ]   
+then
+  exit 1
+fi
+
+exit 0
