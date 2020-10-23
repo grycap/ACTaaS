982699c8b9450e6acf4c97e86c929c2c9285b6b7
marramon <61043057+marramon@users.noreply.github.com>
2020-10-23 21:45:03 +0200

    updated version

--- practices/P3/Face/unittests/run_test_exercise5.sh
+++ practices/P3/Face/unittests/run_test_exercise5.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+cp ./exercise5/*.txt .
+
 sh /var/tmp/education/ACTaaS/practices/P3/Face/unittests/test_exercise5.sh "J. Damian Segrelles Quilis" "Informatica" 7.2
 if [ $? -eq 1 ]   
 then
