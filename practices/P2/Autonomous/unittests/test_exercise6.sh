ed38916c843760c2a61c2a630aaf1caa48e8b1d4
marramon <61043057+marramon@users.noreply.github.com>
2020-10-23 04:58:07 +0200

    Update test_exercise6.sh

--- practices/P2/Autonomous/unittests/test_exercise6.sh
+++ practices/P2/Autonomous/unittests/test_exercise6.sh
@@ -2,7 +2,7 @@
 ./exercise6_bin > sol_ex6_alu.txt
 if  cmp -s /var/tmp/education/ACTaaS/practices/P2/Autonomous/unittests/sol_ex6.txt sol_ex6_alu.txt; then
     echo "Test OK!!"
-    rm sol_ex6_alu.txt
+    rm sol_ex6_alu.txt employee.txt exercise6.c
     exit 0
 else
    echo "Test ERROR!!"
@@ -10,7 +10,7 @@ else
    cat /var/tmp/education/ACTaaS/practices/P2/Autonomous/unittests/sol_ex6.txt
    echo "STUDENT OUTPUT:"
    cat sol_ex6_alu.txt
-   rm sol_ex6_alu.txt
+   rm sol_ex6_alu.txt employee.txt exercise6.c
    exit 1
 fi
 
