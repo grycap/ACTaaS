51c0d3ce3a05b575080f62b93228de4919d609ad
marramon <61043057+marramon@users.noreply.github.com>
2020-10-26 10:20:13 +0100

    updated version

--- practices/P3/Autonomous/unittests/run_test_exercise2.sh
+++ practices/P3/Autonomous/unittests/run_test_exercise2.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cp ./exercise4/*.txt .
-sh /var/tmp/education/ACTaaS/practices/P3/Autonomous/unittests/test_exercise2.sh 1304.7 1900.5 800.65
+sh /var/tmp/education/ACTaaS/practices/P3/Autonomous/unittests/test_exercise2.sh 8000 15000 89500.60
 
 if [ $? -eq 1 ]   
 then
--- practices/P3/Autonomous/unittests/test_exercise2.sh
+++ practices/P3/Autonomous/unittests/test_exercise2.sh
@@ -5,64 +5,22 @@ if [ $# -ne 3 ]; then
    exit
 fi
 
-
-
 echo "$1\n$2\n$3" > spending.txt
-spend=$(cat spending.txt|head -n3|sort -g)
-
 ./exercise2_bin
-pid=$(echo $$)
-proc=$(ps aux|grep $pid)
-test=$(echo "$proc"|grep -oE spending.txt)
-echo "$test" 
-touch sal.txt
-for i in $spend
-do
-   echo $i >> sal.txt
-done
-
-first=$(cat sal.txt|head -n1)
-last=$(cat sal.txt|tail -n1)
-min=$(grep -n $first spending.txt|head -c 1)
-max=$(grep -n $last spending.txt|head -c 1)
-
-case $min in
-  1) mmin="Month during which the minimum spending has occurred: 7"
-     ;;
-  2) mmin="Month during which the minimum spending has occurred: 8"
-     ;;
-  3) mmin="Month during which the minimum spending has occurred: 9"
-esac
-
-case $max in
-  1) mmax="Month during which the maximum spending has occurred: 7"
-     ;;
-  2) mmax="Month during which the maximum spending has occurred: 8"
-     ;;
-  3) mmax="Month during which the maximum spending has occurred: 9"
-esac
-
-cat spending.txt|head -n3 > spending2.txt
-echo "$mmax" >> spending2.txt
-echo "$mmin" >> spending2.txt
-
-pro=$(cat spending2.txt)
-alu=$(cat spending.txt)
-
-if [ "$pro" = "$alu" ]
+if cmp -s spending.txt /var/tmp/education/ACTaaS/practices/P3/Autonomous/unittests/spending.txt 
 then
-   echo "Test OK!!"
-   exit_code=0
+    echo "Test OK!!"
+	exit_code = 0
 else
-  echo "Test ERROR!!"
-  echo "EXPECTED OUTPUT:"
-  echo "$pro"
-  echo "STUDENT OUTPUT:"
-  echo "$alu"
-  exit_code=1  
+   echo "Test ERROR!!"
+   exit_code = 1
+   echo "EXPECTED OUTPUT:"
+   cat /var/tmp/education/ACTaaS/practices/P3/Autonomous/unittests/spending.txt
+   echo "STUDENT OUPUT:"
+   cat spending.txt
 fi
-rm sal.txt spending2.txt
 exit $exit_code
+
 
 
 
