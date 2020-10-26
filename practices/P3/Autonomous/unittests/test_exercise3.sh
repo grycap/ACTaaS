4f09b833a9559fe752c69c5a8810a4eda40e8528
marramon <61043057+marramon@users.noreply.github.com>
2020-10-26 12:26:04 +0100

    Update test_exercise3.sh

--- practices/P3/Autonomous/unittests/test_exercise3.sh
+++ practices/P3/Autonomous/unittests/test_exercise3.sh
@@ -31,7 +31,7 @@ done
 ./exercise3_bin < param.txt > sal.txt
 sal=$(cat sal.txt|grep -oE $total)
 sw=$(echo "$total == $sal"|bc -l)
-if [ sw -eq 1 ]
+if [ $sw -eq 1 ]
 then
    echo "Test OK!!"
    exit_code=0
