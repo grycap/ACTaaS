d7cc68a6d247f1cdd42f3b8181ce026657bdf632
marramon <61043057+marramon@users.noreply.github.com>
2020-10-26 10:46:24 +0100

    Update test_exercise2.sh

--- practices/P3/Autonomous/unittests/test_exercise2.sh
+++ practices/P3/Autonomous/unittests/test_exercise2.sh
@@ -4,6 +4,11 @@ if [ $# -ne 3 ]; then
    echo "$0 <july expenses> <august expenses> <september expenses>"
    exit
 fi
+if [ ! -f spending.txt ]
+then
+   echo "Test ERROR -- File spending.txt not found"
+   exit 1
+fi
 
 echo "$1" > spending.txt
 echo "$2" >> spending.txt
