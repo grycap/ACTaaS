e2e2d9c6e27dfdc6819230391fd5fb4f00229018
marramon <61043057+marramon@users.noreply.github.com>
2020-10-26 16:36:15 +0100

    Update run_test_exercise5.sh

--- practices/P3/Autonomous/unittests/run_test_exercise5.sh
+++ practices/P3/Autonomous/unittests/run_test_exercise5.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-for i in 6959 7323 7764 2517 1002
+for i in 6959 7323 4764 2517 1002
 do
+   echo "Testing for... $i"
    sh /var/tmp/education/ACTaaS/practices/P3/Autonomous/unittests/test_exercise5.sh $i
    if [ $? -eq 1 ]   
    then
