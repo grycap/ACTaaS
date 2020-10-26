91f3922fa238e14b9ea8f7a8424bf05df2c0e4bc
marramon <61043057+marramon@users.noreply.github.com>
2020-10-26 18:15:09 +0100

    updated version

--- practices/P3/Autonomous/unittests/run_test_exercise7.sh
+++ practices/P3/Autonomous/unittests/run_test_exercise7.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+cp ./exercise7/*.txt .
 sh /var/tmp/education/ACTaaS/practices/P3/Autonomous/unittests/test_exercise7.sh
 
+if [ $? -eq 1 ]   
+then
+  exit 1
+fi
 
+exit 0
--- practices/P3/Autonomous/unittests/test_exercise7.sh
+++ practices/P3/Autonomous/unittests/test_exercise7.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+if [ ! -f data.txt ]
+then
+   echo "Test ERROR -- File data.txt not found!"
+   exit 1
+fi
+
 x=$(cat data.txt|head -n1)
 y=$(cat data.txt|tail -n1)
 res=$(echo "$x * $y"|bc)
@@ -10,13 +18,25 @@ else
 fi
 
 ./exercise7_bin
-
+if [ ! -f result.txt ]
+then
+   echo "Test ERROR -- File result.txt not found!"
+   exit 1
+fi
 if grep -q "$pro" "result.txt"
 then
    echo "Test OK!!"
+   exit_code=0
 else
    echo "Test ERROR!!"
+   echo "EXPECTED OUTPUT:"
+   echo "$pro"
+   echo "STUDENT OUTPUT:"
+   cat result.txt
+   exit_code=1
 fi
+rm result.txt data.txt
+exit $exit_code
 
 
 
