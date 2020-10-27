4115ca15ab59c980e0b41206affa717423766f03
marramon <61043057+marramon@users.noreply.github.com>
2020-10-27 07:57:08 +0100

    Create test_exercise5.sh

---
+++ practices/P2/Face/unittests/test_exercise5.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+if [ ! -f items.txt ]; then
+    echo "Test ERROR -- File items.txt found!"
+    exit 1
+fi
+
+nl=$(cat items.txt|wc -l)
+if [ $nl -eq 1 ]; then
+    echo "Test ERROR -- File items.txt seems to be empty"
+    rm items.txt
+    exit 1
+fi
+
+if [ $nl -neq 3 ]; then
+    echo "Test ERROR -- File items.txt must have 3 lines of text check possible blank lines"
+    rm items.txt
+    exit 1
+fi
+
+
+./exercise5_bin
+nl=$(cat items2.txt|wc -l)
+if [ $nl -eq 1 ]; then
+    echo "Test ERROR -- File items2.txt seems to be empty"
+    rm items.txt
+    exit 1
+fi
+counter=0
+filename=items.txt
+dos2unix $filename > /dev/null
+while  read line
+do
+  let counter=$counter+1   
+  printf "%d.- %s\n" $counter "$line" >> items2.copy
+done < "$filename"
+
+if cmp -s items2.txt items2.copy ; then
+   echo "Test OK!!"
+   rm items.txt items2.copy items2.txt
+   exit 0
+else
+   echo "Test ERROR!!"
+   echo "EXPECTED OUTPUT:"
+   cat items2.copy
+   echo "STUDENT OUTPUT:"
+   cat items2.txt
+   rm items.txt items2.copy items2.txt
+   exit 1
+   
+fi
+
