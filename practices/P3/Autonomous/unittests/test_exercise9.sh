e3ba7a364ef725ef7da1f7db548642974ec356fb
marramon <61043057+marramon@users.noreply.github.com>
2020-10-28 07:39:13 +0100

    Update test_exercise9.sh

--- practices/P3/Autonomous/unittests/test_exercise9.sh
+++ practices/P3/Autonomous/unittests/test_exercise9.sh
@@ -11,7 +11,7 @@ vt1=${array[1]}
 vt2=${array[2]}
 tt1=${array[3]}
 tt2=${array[4]}
-#dt1=$(echo "$vt1 * $tt1"|bc -l)
+dt1=$(echo "$vt1 * $tt1"|bc -l)
 #dt2=$(echo "$vt2 * $tt2"|bc -l)
 sw=0 #$(echo "($dt1 + $dt2) < $d"|bc -l)
 if [ $sw -eq 1 ]
