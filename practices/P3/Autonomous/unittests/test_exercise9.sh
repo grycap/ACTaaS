52e2cc9ea65fed414e2f3630af18c6103a8011eb
marramon <61043057+marramon@users.noreply.github.com>
2020-10-28 08:17:10 +0100

    Update test_exercise9.sh

--- practices/P3/Autonomous/unittests/test_exercise9.sh
+++ practices/P3/Autonomous/unittests/test_exercise9.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+if [ $# -ne 5 ]
+then
+   echo "$0 <distance> <const vel of train1> <const vel of train2> <time train 1> <time train 2>"
+   echo "All measurements must be in the international system of units."
+   exit
+fi
+d=$1
+vt1=$2
+vt2=$3
+tt1=$4
+tt2=$5
+for data in $@
+do
+   echo $data >> train.txt
+done
 alu=$(./exercise9_bin|tr -d '\n')
-array=()
-while IFS= read -r line; do
-   array+=("$line")
-done < train.txt
 
-d=${array[0]}
-vt1=${array[1]}
-vt2=${array[2]}
-tt1=${array[3]}
-tt2=${array[4]}
 dt1=$(echo "$vt1 * $tt1"|bc -l)
 dt2=$(echo "$vt2 * $tt2"|bc -l)
 sw=0 #$(echo "($dt1 + $dt2) < $d"|bc -l)
@@ -32,4 +38,5 @@ else
    echo "STUDENT OUTPUT:"
    exit_code=1
 fi
+rm train.txt
 exit $exit_code
