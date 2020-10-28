0c276874b9d4bc912c33af6fab23b58d41865c11
marramon <61043057+marramon@users.noreply.github.com>
2020-10-28 06:50:10 +0100

    updated version

---
+++ practices/P3/Autonomous/unittests/run_test_exercise9.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cp /var/tmp/education/ACTaaS/practices/P3/Autonomous/solutions/train.txt .
+/bin/bash /var/tmp/education/ACTaaS/practices/P3/Autonomous/unittests/test_exercise9.sh
+
+if [ $? -eq 1 ]   
+then
+  exit 1
+fi
+
+exit 0
