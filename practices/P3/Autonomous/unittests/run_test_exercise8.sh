e84ebda2b6021bb440316d5f11de412811cf8a26
marramon <61043057+marramon@users.noreply.github.com>
2020-10-28 07:00:25 +0100

    Update run_test_exercise8.sh

--- practices/P3/Autonomous/unittests/run_test_exercise8.sh
+++ practices/P3/Autonomous/unittests/run_test_exercise8.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp /var/tmp/education/ACTaaS/practices/P3/Autonomous/solutions/info.txt .
-/bin/bash /var/tmp/education/ACTaaS/practices/P3/Autonomous/unittests/test_exercise8.sh 25
+sh /var/tmp/education/ACTaaS/practices/P3/Autonomous/unittests/test_exercise8.sh 25
 
 if [ $? -eq 1 ]   
 then
