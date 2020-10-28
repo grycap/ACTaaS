d418802effe164dedb253ff17637f33079c41af1
marramon <61043057+marramon@users.noreply.github.com>
2020-10-28 08:22:58 +0100

    Update run_test_exercise9.sh

--- practices/P3/Autonomous/unittests/run_test_exercise9.sh
+++ practices/P3/Autonomous/unittests/run_test_exercise9.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cp /var/tmp/education/ACTaaS/practices/P3/Autonomous/solutions/train.txt .
-sh /var/tmp/education/ACTaaS/practices/P3/Autonomous/unittests/test_exercise9.sh
+sh /var/tmp/education/ACTaaS/practices/P3/Autonomous/unittests/test_exercise9.sh 20000 83.3333 38.8889 80.20 58.14
 
 if [ $? -eq 1 ]   
 then
