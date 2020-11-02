21d3854b94bb311e5cedea976bcbd00ed0c6b673
marramon <61043057+marramon@users.noreply.github.com>
2020-11-02 13:45:17 +0100

    Update run_test_exercise3.sh

--- practices/P4/Face/unittests/run_test_exercise3.sh
+++ practices/P4/Face/unittests/run_test_exercise3.sh
@@ -1,3 +1,13 @@
+echo "Testing for n = 512"
+sh /var/tmp/education/ACTaaS/practices/P4/Face/unittests/test_exercise3.sh 512
+if [ $? -eq 1 ]   
+then
+  exit 1
+else
+  exit 0
+fi
+
+echo "Testing for n = 31" # Prime number
 sh /var/tmp/education/ACTaaS/practices/P4/Face/unittests/test_exercise3.sh 512
 if [ $? -eq 1 ]   
 then
