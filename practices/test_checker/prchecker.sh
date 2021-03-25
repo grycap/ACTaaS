471bcc582645f1efe87dcfc4aa0c31b8214df5c6
Marcos <marcramosmontes@gmail.com>
2021-03-25 18:02:47 +0100

    Update prchecker.sh

--- practices/test_checker/prchecker.sh
+++ practices/test_checker/prchecker.sh
@@ -19,19 +19,19 @@ test_OK() {
        local c=0
 	   for f in $list
        do
-	    local exit_status=0
+	    local exit_status=1
         printf "\nProcessing source code ${f##*/}..."
         gcc -Wall -Wno-main -lm $f -o exercise${JOB_NAME}_bin 2> gcclog.txt
         if [ -s gcclog.txt ]  
         then
            printf " Compilation error in ${f##*/}" 
-		   exit_status=1
+		   exit_status=0
         else
            sh /var/tmp/education/ACTaaS/practices/P${PRACTICA}/$TYPE/unittests/run_test_exercise${JOB_NAME}.sh >> /dev/null 2>&1
            if [ $? -ne 0 ]   
            then
               printf " False positive in ${f##*/}"
-			  exit_status=1
+			  exit_status=0
 		   fi
 	    fi
         rm ./gcclog.txt
